e729ccc19aa5bcc1d53767fdb8ea27376cccbe15
Supragya Shrestha <supragyashrestha02@gmail.com>
2020-02-05 22:24:10 +0530

    hosting

---
+++ publications.js
@@ -0,0 +1,21 @@
+$(window).on('load',function() { //when the document is loaded
+	$(".preload").delay(2000).fadeOut("slow"); //fade out preloader
+  });
+  
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
